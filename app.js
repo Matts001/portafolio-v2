c39f41655b5c7e49014046a6e029fd193cc4ba48
Matts001 <benitez69068405@gmail.com>
2022-07-05 16:02:14 -0400

    update

--- app.js
+++ app.js
@@ -13,3 +13,51 @@ e.preventDefault();
         behavior: "smooth",
     });
 }
+
+//text writing
+
+const typedWord = document.querySelector(".writing");
+
+const wordArray = ["Web Developer", "Digital Artist"];
+let wordArrayIndex = 0;
+let letterIndex = 0;
+
+const typingDelay = 200;
+const erasingDelay = 100;
+const newWordDelay = 1000;
+
+// Typing Function
+function type() {
+  if (letterIndex < wordArray[wordArrayIndex].length) {
+
+    typedWord.textContent += wordArray[wordArrayIndex].charAt(letterIndex);
+    letterIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newWordDelay);
+  }
+}
+
+// Erase Function
+function erase() {
+  if (letterIndex > 0) {
+    
+    typedWord.textContent = wordArray[wordArrayIndex].substring(
+      0,
+      letterIndex - 1
+    );
+    letterIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+
+    wordArrayIndex++;
+    if (wordArrayIndex >= wordArray.length) {
+      wordArrayIndex = 0;
+    }
+    setTimeout(type, typingDelay);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(type, newWordDelay);
+});
